9c76cccd89eb6daa1db7bece2189f663a6c537d5
Aaryan Raj <aryan.raj000@outlook.com>
2016-01-06 01:03:44 +0530

    change db in prod

--- config/env/production.js
+++ config/env/production.js
@@ -8,10 +8,10 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' +'ds039185.mongolab.com:39185'+ '/mean-dev000',
     options: {
-      user: '',
-      pass: ''
+      user: 'tester000',
+      pass: 'codenotbroken'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
